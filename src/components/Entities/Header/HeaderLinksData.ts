5a05c3e0b45db5a3c65c15b3ca38d2e624bf54a3
miguelsgil <jorelwall154@gmail.com>
2025-04-17 11:09:23 -0300

    Add HeaderLinksData

---
+++ src/components/Entities/Header/HeaderLinksData.ts
@@ -0,0 +1,22 @@
+
+interface ILink {
+    name: string;
+    href: string;
+}
+
+const headerLinksData: ILink[] = [
+    {
+        name: "Sobre",
+        href: "#sobre"
+    },
+    {
+        name: "Projetos",
+        href: "#projetos"
+    },
+    {
+        name: "Contato",
+        href: "#contato"
+    }
+];
+
+export default headerLinksData;
