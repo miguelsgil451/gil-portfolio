03dd35c7c1c9b1c4603f260971224c0616290946
miguelsgil <jorelwall154@gmail.com>
2025-04-17 11:08:13 -0300

    create main.tsx and app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Article from "./components/Entities/Article";
+import Banner from "./components/Entities/Banner";
+import Footer from "./components/Entities/Footer";
+import Header from "./components/Entities/Header/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Article />
+      <Footer />
+    </>
+  )
+}
