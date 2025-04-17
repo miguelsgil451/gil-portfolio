3b57addab3cf8bed4ade41d8505bfb0195194e68
miguelsgil <jorelwall154@gmail.com>
2025-04-17 11:08:33 -0300

    Add logo component

---
+++ src/components/Base/Logo.tsx
@@ -0,0 +1,6 @@
+
+export default function Logo(){
+    return (
+        <h1 className="text-3xl font-semibold font-mono"><span className="text-4xl">&lt;</span><span className="text-4xl text-emerald-300">Gil</span >Code<span className="text-4xl">/&gt;</span></h1>
+    );
+}
